f83c9536d385c250e3af0814a190211a3017c5ad
RNHATER <sfrancov@unal.edu.co>
2022-04-30 15:28:32 -0500

    Implementación de pruebas de fisher

--- Informe1/lectura_datos.r
+++ Informe1/lectura_datos.r
@@ -1,7 +1,9 @@
 library(readxl)
+library(dplyr)
 
 #Lectura de datos
 datos_encuestas <- read_excel('resultados_categoricos.xlsx')
+datos_encuestas
 
 #Generacion de bases de datos
 tablas <- list()
@@ -14,3 +16,37 @@ for(i in 1:10){
     tablas[[i]] <- datos
 }
 tablas
+
+tabla_general <- datos_encuestas %>% 
+    group_by(Genero, Belleza) %>% summarise(Resultado =sum(Resultado))
+tabla_general <- t(matrix(tabla_general$Resultado, nrow = 2, ncol = 2))
+tabla_general <- tabla_general[c(2,1),]
+row.names(tabla_general) <- c('Mujer', 'Hombre')
+colnames(tabla_general) <- c('Bonito', 'Feo')
+
+tabla_mujeres <- datos_encuestas[datos_encuestas['Genero-Encuestado']=='F',] %>% 
+    group_by(Genero, Belleza) %>% summarise(Resultado =sum(Resultado))
+tabla_mujeres <- t(matrix(tabla_mujeres$Resultado, nrow = 2, ncol = 2))
+row.names(tabla_mujeres) <- c('Mujer', 'Hombre')
+colnames(tabla_mujeres) <- c('Bonito', 'Feo')
+
+tabla_hombres <- datos_encuestas[datos_encuestas['Genero-Encuestado']=='M',] %>% 
+    group_by(Genero, Belleza) %>% summarise(Resultado =sum(Resultado))
+tabla_hombres <- t(matrix(tabla_hombres$Resultado, nrow = 2, ncol = 2))
+tabla_hombres <- tabla_hombres[c(2,1),]
+row.names(tabla_hombres) <- c('Mujer', 'Hombre')
+colnames(tabla_hombres) <- c('Bonito', 'Feo')
+
+##Pruebas de fisher sobre tablas
+
+#Tabla general
+fisher.test(tabla_general)
+"Se concluye que la selección es estrictamente aleatoria"
+
+#Tabla mujeres
+fisher.test(tabla_mujeres)
+"Se concluye que la selección es estrictamente aleatoria"
+
+#Tabla hombres
+fisher.test(tabla_hombres)
+"Se concluye que la selección es estrictamente aleatoria"
